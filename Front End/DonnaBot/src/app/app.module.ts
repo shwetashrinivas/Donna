8867331ce6cc36dce631e476e44f9560297d9980
Shweta Shrinivas <shshri31@in.ibm.com>
2020-09-11 22:23:39 +0530

    Added basic components

--- Front End/DonnaBot/src/app/app.module.ts
+++ Front End/DonnaBot/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatWidgetComponent } from './components/chat-widget/chat-widget.component';
+import { ChatPopupComponent } from './components/chat-popup/chat-popup.component';
+import { ChatNavbarComponent } from './components/chat-navbar/chat-navbar.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ChatFormComponent } from './components/chat-form/chat-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWidgetComponent,
+    ChatPopupComponent,
+    ChatNavbarComponent,
+    ChatWindowComponent,
+    ChatFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front End/DonnaBot/src/app/components/chat-popup/chat-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-popup',
+  templateUrl: './chat-popup.component.html',
+  styleUrls: ['./chat-popup.component.css']
+})
+export class ChatPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
